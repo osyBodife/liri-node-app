fee0731a51c1ae32c71cf56364ff77be595f86e8
Osy odife <osyodife@gmail.com>
2019-08-08 11:15:31 -0400

    upload modified ,js files

--- keys.js
+++ keys.js
@@ -7,8 +7,7 @@ exports.spotify = {
   secret: process.env.SPOTIFY_SECRET
 };
 
-//Client ID 09b0a9b9b6e049aea77157b9e5434742
-//Client Secret 638a49456f3842949c56f156b3dbdd9d 
+
 
 exports.bandsintown = {
   id: process.env.BANDSINTOWN_ID
--- liri.js
+++ liri.js
@@ -7,135 +7,173 @@ var keys = require("./keys.js");
 var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
 
+//console.log(process.argv);
+let action = process.argv[2];
+//from the process.argv array 
+// create an new array starting from 3rd item
+//convert to a string
+//reomve all the commas
+let searchTerm = process.argv.slice(3).toString().replace(/,/g, " ");
+
+console.log(action);
+console.log(searchTerm);
+
+var axios = require("axios");
+
+
+
+//REQUIRE FILE SYSTEMS
+const fs = require("fs");
+
+
 // OMDB AND BANDS IN TOWN API'S
 let omdb = (keys.omdb);
 let bandsintown = (keys.bandsintown);
 
 
-//console.log(process.argv);
-let action =process.argv[2];
-//from the process.argv array 
-// create an new array starting from 3rd item
-//convert to a string
-//reomve all the commas
-let searchTerm=process.argv.slice(3).toString().replace(/,/g , " ");
-
-console.log(action);
-console.log(searchTerm);
-
-
 function doThis() {
-    // UTILIZE THE BUILT IN READFILE METHOD TO ACCESS RANDOM.TXT
-    fs.readFile("random.txt", "utf8", function (error, data) {
-        if (error) {
-            return console.log(error);
-        }
-        // create an array from the data based the on separator "," using the split()
-        let dataArray = data.split(",");
-		console.log ("Contents of our array :" + dataArray)
-		
+  // UTILIZE THE BUILT IN READFILE METHOD TO ACCESS RANDOM.TXT
+  fs.readFile("random.txt", "utf8", function (error, data) {
+    if (error) {
+      return console.log(error);
+    }
+    // create an array from the data based the on separator "," using the split()
+    let dataArray = data.split(",");
+    console.log("Contents of our array :" + dataArray)
 
-        // use the array elements as input parameters for userCommand function
-       action = dataArray[0];
-        searchTerm = dataArray[1];
-        // call the userCommand function
-        userCommand(action, searchTerm);
-    });
+
+    // use the array elements as input parameters for userCommand function
+    action = dataArray[0];
+    searchTerm = dataArray[1];
+    // call the userRequest function
+    userRequest(action, searchTerm);
+  });
 };
-//Send all data requests to:
 
-////http://www.omdbapi.com/?apikey=[yourkey]&
-//you can now return all episodes by using just the "Season" parameter: http://www.omdbapi.com/?t=Game of Thrones&Season=1
-/*
-//let searchTerm = "Game of Thrones&Season";
-let queryURL="http://www.omdbapi.com/?i=tt3896198&apikey=bf76ed6a&t=" + searchTerm + "&Season=1";
-    //let queryURL = "https://api.giphy.com/v1/gifs/search?api_key=lISwnQ5TdGTfXd9Ex4N17L98lbq6KRRi&limit=10&q=" + searchTerm;
-    //make an ajax call
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })// We store all of the retrieved data inside of an object called "response"
-        .then(function (response) {
-        ls
-        console.log(response);
-            //store all data from ajax call to a variable
-            let results = response.data;
-            //alert the user when there no images for current search form giphy.com
-            if(results.length===0){
-                alert("Sorry there is NO giphy images for your current search")
-            }else{
-            //loop thru entire results
-            for (var i = 0; i < results.length; i++) {
-                // create a div where to container each image
 
-            }
-        }
-    } );
+//axios.get("https://rest.bandsi}ntown.com/artists/" + searchTerm  + "/events?app_id=" + bandsintown).then(
+function getBandsInTown() {
+  axios.get("https://rest.bandsintown.com/artists/" + searchTerm + "/events?app_id=codingbootcamp").then(
+    function (response) {
+      // If the axios was successful...
+      // Then log the body from the site!
+      console.log(response.data);
+      //console.log(response.name);
+      //console.log(response.data[0].name);
+      //console.log(response.data.offers.venue.name);
+    },
 
-*/
-/* movie function
-    ///Run the axios.get function...
-// The axios.get function takes in a URL and returns a promise (just like $.ajax)
-axios.get("http://www.omdbapi.com/?i=tt3896198&apikey=bf76ed6a&t=" + searchTerm ).then(
-  function(response) {
-    // If the axios was successful...
-    // Then log the body from the site!
-    console.log(response.data);
-  },
+    function (error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an object that comes back with details pertaining to the error that occurred.
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
+    });
+}
 
-  function(error) {
-    if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      console.log(error.response.data);
-      console.log(error.response.status);
-      console.log(error.response.headers);
-    } else if (error.request) {
-      // The request was made but no response was received
-      // `error.request` is an object that comes back with details pertaining to the error that occurred.
-      console.log(error.request);
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      console.log("Error", error.message);
+function spotifyThis() {
+  console.log(`\n - - - - -\n\nSEARCHING FOR..."${searchTerm}"`);
+
+  // IF USER QUERY NOT FOUND, PASS VALUE OF "ACE OF BASE" 
+  if (!searchTerm) {
+    searchTerm = "Wrong search term"
+  };
+
+  // SEARCH with spotify format
+
+  spotify.search({
+    type: 'track',
+    query: searchTerm,
+    limit: 2
+  }, function (error, data) {
+    if (error) {
+      return console.log('Error occurred: ' + error);
     }
-    console.log(error.config);
+    // COLLECT SELECTED DATA IN AN ARRAY
+    let spotifyArray = data.tracks.items;
+
+    for (i = 0; i < spotifyArray.length; i++) {
+      console.log(`\nBA DA BOP!  That's for you...\n\nArtist: ${data.tracks.items[i].album.artists[0].name} \nSong: ${data.tracks.items[i].name}\nAlbum: ${data.tracks.items[i].album.name}\nSpotify link: ${data.tracks.items[i].external_urls.spotify}\n\n - - - - -`)
+    };
   });
 
-*/
-//https://rest.bandsintown.com/artists/" + argument + "/events?app_id=codingbootcamp"
-    ///Run the axios.get function...
+}
+
+
+
+
+//movie function
+///Run the axios.get function...
 // The axios.get function takes in a URL and returns a promise (just like $.ajax)
+function getMovie() {
+  axios.get("http://www.omdbapi.com/?i=tt3896198&apikey=bf76ed6a&t=" + searchTerm).then(
+    function (response) {
+      // If the axios was successful...
+      // Then log the body from the site!
+      console.log(response.data);
+      console.log(" Movie Title :" + response.data.Title);
+      console.log(" Year of Release :" + response.data.Year);
+      console.log("The  imdbRating :" + response.data.imdbRating);
+      //console.log("The Rotten Tomatoes Rating :" + response.data.imdbRating);
+      console.log("The  country of release :" + response.data.Country);
+      console.log("The  Language of the movie:" + response.data.Language);
+      console.log("The  Plot :" + response.data.Plot);
+      console.log("The  Actors :" + response.data.Actors);
 
-//axios.get("https://rest.bandsi}ntown.com/artists/" + searchTerm  + "/events?app_id=" + bandsintown, function (error, response, body) {
+    },
+    function (error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an object that comes back with details pertaining to the error that occurred.
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
+    });
+}
 
-//});
 
 
-//axios.get("https://rest.bandsintown.com/artists/" + searchTerm + "/events?app_id=codingbootcamp" ).then(
-  axios.get("https://rest.bandsi}ntown.com/artists/" + searchTerm  + "/events?app_id=" + bandsintown).then(
-  function(response) {
-    // If the axios was successful...
-    // Then log the body from the site!
-    console.log(response.data);
-    //console.log(response.name);
-    //console.log(response.data[0].name);
-    //console.log(response.data.offers.venue.name);
-  },
+// the finally Liri code logic
+function userRequest(action, searchTerm) {
+  // make a decision based on the command
+  switch (action) {
+    case "concert-this":
+      //concertThis();
+      getBandsInTown();
+      break;
+    case "spotify-this":
+      spotifyThis();
+      break;
+    case "movie-this":
+      getMovie();
+      break;
+    case "do-this":
+      doThis(searchTerm);
+      break;
+    default:
+      console.log("I don't understand");
+      break;
+  }
+}
 
-  function(error) {
-    if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      console.log(error.response.data);
-      console.log(error.response.status);
-      console.log(error.response.headers);
-    } else if (error.request) {
-      // The request was made but no response was received
-      // `error.request` is an object that comes back with details pertaining to the error that occurred.
-      console.log(error.request);
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      console.log("Error", error.message);
-    }
-    console.log(error.config);
-  });
+userRequest(action, searchTerm);
